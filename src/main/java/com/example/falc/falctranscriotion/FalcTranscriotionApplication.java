1a823d526360b72c19d93ec30ee003c8b6dad472
GeekHamza <med.hamza.attar@gmail.com>
2024-07-08 11:17:02 +0200

    Falc transcrition prject ==> Inital commit

---
+++ src/main/java/com/example/falc/falctranscriotion/FalcTranscriotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.falc.falctranscriotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalcTranscriotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FalcTranscriotionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/falc/falctranscriotion/FalcTranscriotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.falc.falctranscriotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalcTranscriotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
